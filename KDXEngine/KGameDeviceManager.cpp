923cce5645f403956b7c960c1756595e43cdf79e
kwon3000hero <kwon3000hero@outlook.com>
2021-08-11 22:16:26 +0900

    2021-08-11-0
    DeviceWrapper0에 대한 처리 완료

--- KDXEngine/KGameDeviceManager.cpp
+++ KDXEngine/KGameDeviceManager.cpp
@@ -2,20 +2,21 @@
 
 #include "KGameDeviceManager.h"
 #include "KRenderTarget.h"
+#include "KGameDevice.h"
 
-Microsoft::WRL::ComPtr<ID3D11Device> KGameDeviceManager::m_pMainDevice = nullptr;
-Microsoft::WRL::ComPtr<ID3D11DeviceContext> KGameDeviceManager::m_pMainContext = nullptr;
+Microsoft::WRL::ComPtr<DeviceBuildVersion::DeviceType> KGameDeviceManager::m_pMainDevice = nullptr;
+Microsoft::WRL::ComPtr<DeviceBuildVersion::ContextType> KGameDeviceManager::m_pMainContext = nullptr;
 
 KGameDevice<DeviceBuildVersion>* KGameDeviceManager::m_pMainGameDevice = nullptr;
 
 std::map<KGameString, KPTR<KGameDevice<DeviceBuildVersion>>> KGameDeviceManager::m_deviceContainer;
 
-Microsoft::WRL::ComPtr<ID3D11Device> KGameDeviceManager::MainDevice()
+Microsoft::WRL::ComPtr<DeviceBuildVersion::DeviceType> KGameDeviceManager::MainDevice()
 {
 	return m_pMainDevice;
 }
 
-Microsoft::WRL::ComPtr<ID3D11DeviceContext> KGameDeviceManager::MainContext()
+Microsoft::WRL::ComPtr<DeviceBuildVersion::ContextType> KGameDeviceManager::MainContext()
 {
 	return m_pMainContext;
 }
@@ -24,12 +25,12 @@ KGameDevice<DeviceBuildVersion>* KGameDeviceManager::MainGameDevice()
 	return m_pMainGameDevice;
 }
 
-void KGameDeviceManager::SetMainDevice(Microsoft::WRL::ComPtr<ID3D11Device> _device)
+void KGameDeviceManager::SetMainDevice(Microsoft::WRL::ComPtr<DeviceBuildVersion::DeviceType> _device)
 {
 	m_pMainDevice = _device;
 }
 
-void KGameDeviceManager::SetMainContext(Microsoft::WRL::ComPtr<ID3D11DeviceContext> _Context)
+void KGameDeviceManager::SetMainContext(Microsoft::WRL::ComPtr<DeviceBuildVersion::ContextType> _Context)
 {
 	m_pMainContext = _Context;
 }
@@ -44,4 +45,28 @@ void KGameDeviceManager::SetMainRenderTarget()
 {
 	m_pMainGameDevice->MainTarget->Clear();
 	m_pMainGameDevice->MainTarget->Setting();
+}
+
+KPTR<KGameDevice<DeviceBuildVersion>> KGameDeviceManager::Find(const KGameString& _DeviceName)
+{
+	if (m_deviceContainer.end() == m_deviceContainer.find(_DeviceName))
+	{
+		return nullptr;
+	}
+
+	return m_deviceContainer[_DeviceName];
+}
+
+void KGameDeviceManager::Create(const KGameString& _WindowName, KVector _ClearColor)
+{
+	if (nullptr != Find(_WindowName).get())
+	{
+		assert(false);
+	}
+
+	KPTR<KGameDevice<DeviceBuildVersion>> device = new KGameDevice<DeviceBuildVersion>();
+	device->Create(KGameWindow::FindWin(_WindowName), _ClearColor);
+
+	m_deviceContainer.insert(
+		std::map<KGameString, KPTR<KGameDevice<DeviceBuildVersion>>>::value_type(_WindowName, device));
 }
--- KDXEngine/KGameDeviceManager.h
+++ KDXEngine/KGameDeviceManager.h
@@ -1,55 +1,39 @@
 #pragma once
 #include <wrl.h>
 #include <d3dcompiler.h>
-#include "KGameDevice.h"
 #include "KGameReference.h"
+#include "BuildTarget.h"
+#include <map>
 
+template<typename T>
+class KGameDevice;
 class KRenderTarget;
+class KVector;
 class KGameDeviceManager final : public KGameReference
 {
 
 private:
-	static Microsoft::WRL::ComPtr<ID3D11Device> m_pMainDevice;
-	static Microsoft::WRL::ComPtr<ID3D11DeviceContext> m_pMainContext;
+	static Microsoft::WRL::ComPtr<DeviceBuildVersion::DeviceType> m_pMainDevice;
+	static Microsoft::WRL::ComPtr<DeviceBuildVersion::ContextType> m_pMainContext;
 	static KGameDevice<DeviceBuildVersion>* m_pMainGameDevice;
-
-public:
-	static Microsoft::WRL::ComPtr<ID3D11Device> MainDevice();
-	static Microsoft::WRL::ComPtr<ID3D11DeviceContext> MainContext();
-	static KGameDevice<DeviceBuildVersion>* MainGameDevice();
-
-	static void SetMainDevice(Microsoft::WRL::ComPtr<ID3D11Device> _device);
-	static void SetMainContext(Microsoft::WRL::ComPtr<ID3D11DeviceContext> _Context);
-	static void SetMainGameDevice(KGameDevice<DeviceBuildVersion>* _pDevice);
-
-	static void SetMainRenderTarget();
-
 
 private:
 	static std::map<KGameString, KPTR<KGameDevice<DeviceBuildVersion>>> m_deviceContainer;
 
 public:
-	static KPTR<KGameDevice<DeviceBuildVersion>> Find(const KGameString& _DeviceName)
-	{
-		if (m_deviceContainer.end() == m_deviceContainer.find(_DeviceName))
-		{
-			return nullptr;
-		}
+	static Microsoft::WRL::ComPtr<DeviceBuildVersion::DeviceType> MainDevice();
+	static Microsoft::WRL::ComPtr<DeviceBuildVersion::ContextType> MainContext();
+	static KGameDevice<DeviceBuildVersion>* MainGameDevice();
 
-		return m_deviceContainer[_DeviceName];
-	}
+	static void SetMainDevice(Microsoft::WRL::ComPtr<DeviceBuildVersion::DeviceType> _device);
+	static void SetMainContext(Microsoft::WRL::ComPtr<DeviceBuildVersion::ContextType> _Context);
+	static void SetMainGameDevice(KGameDevice<DeviceBuildVersion>* _pDevice);
 
-	static void Create(const KGameString& _WindowName, KVector _ClearColor)
-	{
-		if (nullptr != Find(_WindowName).get())
-		{
-			assert(false);
-		}
+	static void SetMainRenderTarget();
 
-		/*KPTR<KGameDevice<DeviceBuildVersion>> NewDevice = new KGameDevice<DeviceBuildVersion>();
-		NewDevice->Create(KGameWindow::FindWin(_WindowName), _ClearColor);
 
-		m_deviceContainer.insert(
-			std::map<KGameString, KPTR<KGameDevice<DeviceBuildVersion>>>::value_type(_WindowName, NewDevice));*/
-	}
+public:
+	static KPTR<KGameDevice<DeviceBuildVersion>> Find(const KGameString& _DeviceName);
+
+	static void Create(const KGameString& _WindowName, KVector _ClearColor);	
 };
